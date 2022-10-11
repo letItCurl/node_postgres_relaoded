d48a43fb7558bd44b8a4d4b5ab22301099f614bd
letItCurl <rolandlopez.developer@gmail.com>
2022-10-11 11:14:59 +0700

    🌴 Growing.

---
+++ fortunes_api/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fortunes = require('./data/fortunes.json')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/fortunes', (req, res) => {
+  res.send(fortunes)
+})
+
+app.get('/fortunes/random', (req, res) => {
+  const random_index = Math.floor(Math.random() * fortunes.length)
+  const r_fortunes = fortunes[random_index]
+  res.json(r_fortunes)
+})
+
+app.get('/fortunes/:id', (req, res) => {
+  res.json(fortunes.find((f) => f.id == req.params.id))
+})
+
+app.post('/fortunes', (req, res) => {
+  res.send(req.body)
+})
+
+
+module.exports = app
